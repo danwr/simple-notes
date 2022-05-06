e03f9bb9e30117749a6a01ba3b590d048bd42698
Dan Wright <dan@banasmoo.com>
2022-05-05 19:40:18 -0700

    Create dispatch.php

---
+++ app/dispatch.php
@@ -0,0 +1,12 @@
+<?php
+
+use utilities/Dispatcher;
+
+$dispatcher = new Dispatcher();
+
+$dispatcher->dispatchGET('/list', 'NotesController#index');
+$dispatcher->dispatchGET('/delete', 'NotesController#delete');
+$dispatcher->dispatchGET('/new', 'NotesController#insert');
+$dispatcher->dispatchGET('/edit', 'NotesController#edit');
+
+?>
