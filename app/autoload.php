e9317482981b5b28b433059c56d80719d8760d37
Dan Wright <dan@banasmoo.com>
2022-05-05 19:32:22 -0700

    Create autoload.php

---
+++ app/autoload.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Class Autoloader
+ */
+class Autoloader
+{
+    /**
+     * Loads the classes as called.
+     *
+     * @param $class
+     * @return bool
+     */
+    public static function loader($class)
+    {
+        $file = dirname(__DIR__) . '/app/ . str_replace('\\', '/', $class) . '.php';
+        if (file_exists($file)) {
+            include $file;
+
+            if (class_exists($class)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Registers the autoloader for the application.
+ */
+spl_autoload_register('Autoloader::loader');
