bee3b09ad199377e2bbe5c86d52ecc7cd88a9c83
Dan Wright <dan@banasmoo.com>
2022-05-05 19:48:51 -0700

    Create Controller.php

---
+++ app/controllers/Controller.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Controllers;
+
+use config\Database;
+use utilities\Renderer;
+
+/**
+ * Class Controller
+ *
+ * @package Controllers
+ */
+class Controller
+{
+    /**
+     * Controller constructor.
+     */
+    function __construct()
+    {
+    }
+
+    /**
+     * Retrieves the renderer for the child controllers.
+     *
+     * @return Renderer
+     */
+    public function renderer()
+    {
+        return new Renderer();
+    }
+
+    /**
+     * Returns a the database object.
+     *
+     * @return Database
+     */
+    public function database()
+    {
+        return new Database();
+    }
+
+    /**
+     * Redirects the user to the path
+     *
+     * @param $path
+     */
+    public function redirect($path)
+    {
+        return header('Location: ' . $path);
+    }
+}
+?>
