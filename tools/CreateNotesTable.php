bc94bd2a786415223faff6ad1b92f12e42a25266
Dan Wright <dan@banasmoo.com>
2022-05-06 15:23:19 -0700

    Create CreateNotesTable.php

---
+++ tools/CreateNotesTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace tools;
+
+use config\Database;
+
+class CreateNotesTable
+{
+    private $connection;
+    
+    function __construct()
+    {
+        $this->connection = (new Database())->createConnection();
+    }
+    
+    public function create()
+    {
+        $this->connection->exec('CREATE TABLE IF NOT EXISTS notes (
+                id      INTEGER PRIMARY KEY AUTOINCREMENT,
+                title   TEXT NOT NULL,
+                content TEXT NOT NULL,
+                tags    TEXT NOT NULL,
+                creation DATETIME NOT NULL
+                );');
+    }
+    
+    public function drop()
+    {
+        $this->connection->exec('DROP TABLE IF EXISTS notes;');
+    }
+}
+
+?>
