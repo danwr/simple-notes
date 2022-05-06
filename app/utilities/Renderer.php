8408a096d1c249cb823337dfd1f918564ea2f6b2
Dan Wright <dan@banasmoo.com>
2022-05-05 22:33:02 -0700

    Create Renderer.php

---
+++ app/utilities/Renderer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace utilities;
+
+/**
+ * Class Renderer
+ */
+class Renderer
+{
+    private $viewDir;
+
+    /**
+     * Renderer constructor.
+     */
+    function __construct()
+    {
+        $this->viewDir = dirname(__DIR__) . '/Views/';
+    }
+
+    /**
+     * This renders a view and then returns its contents
+     *
+     * @param $view
+     * @param $args
+     * @return string
+     */
+    public function renderView($view, $args)
+    {
+        ob_start();
+        require_once $this->viewDir . $view . '.php';
+        ob_end_flush();
+    }
+}
+?>
