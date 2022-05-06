cdd51fb4091b3d0a472b8fdbbcdcb86e7f52b93e
Dan Wright <dan@banasmoo.com>
2022-05-05 22:41:43 -0700

    Create index.php

---
+++ app/public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Enables autoloading of classes without includes.
+ *
+ * This allows you to just spawn new instances of the classes as needed with out include statements
+ * everywhere. This is great for larger projects where you're orchestrating a lot of different
+ * objects.
+ */
+require_once __DIR__ . '/../app/autoload.php';
+
+/**
+ * All of your web requests are sent to this index file, and then based on what
+ * the URI is, the dispatcher then issues the correct commands to PHP.
+ */
+require_once __DIR__ . '/../app/dispatch.php';
+?>
