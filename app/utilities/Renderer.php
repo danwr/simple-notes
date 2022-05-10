baea7f386b3b18baa7f41b8f53dafcd6ab69f9c5
Dan Wright <dan@danwright.info>
2022-05-09 20:23:52 -0400

    Remove some debug logging

--- app/utilities/Renderer.php
+++ app/utilities/Renderer.php
@@ -26,8 +26,6 @@ class Renderer
      */
     public function renderView($view, $args)
     {
-	printf("renderView(%s) ", $this->viewDir . $view . '.php');
-	var_dump($args);
         ob_start();
         require_once $this->viewDir . $view . '.php';
         ob_end_flush();
